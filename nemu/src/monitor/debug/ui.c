fdfc4faedecaf8840cf923eb9ec5b9591dd61814
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-03 14:31:26 +0000

    >  compile NEMU 3019244410 simpleedu Linux simpleedu 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  14:31:26 up 8 days,  6:25,  1 user,  load average: 1.83, 2.18, 2.40 a341cda74e6fbe5e1278cffd2757e41ab43cc3f0

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -37,7 +37,7 @@ static int cmd_q(char *args) {
 }
 
 static int cmd_help(char *args);
-
+static int cmd_si(char *args);
 static struct {
 	char *name;
 	char *description;
@@ -46,7 +46,7 @@ static struct {
 	{ "help", "Display informations about all supported commands", cmd_help },
 	{ "c", "Continue the execution of the program", cmd_c },
 	{ "q", "Exit NEMU", cmd_q },
-
+        { "si","Single-step execution", cmd_si },
 	/* TODO: Add more commands */
 
 };
@@ -75,6 +75,55 @@ static int cmd_help(char *args) {
 	}
 	return 0;
 }
+static int cmd_si(char *args){
+          char *arg = strtok(NULL, " ");
+          int i = 0, j = 0;
+          if(arg == NULL){
+                 cpu_exec(1);
+                 return 0;
+          }
+          sscanf(arg, "%d", &i);
+          if(i < -1){
+                 printf("Parameter error\n");
+                 return 0;
+          }
+          if(i == -1){
+                 cpu_exec(-1);
+          }
+          for(j = 0;j < i;j++){
+                 cpu_exec(1);
+          }
+          return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
 
 void ui_mainloop() {
 	while(1) {
