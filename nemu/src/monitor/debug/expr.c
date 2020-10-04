b78c2bdca4b940e7e2075bb57ab05785032d5514
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-04 05:30:37 +0000

    >  compile NEMU 3019244410 simpleedu Linux simpleedu 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  05:30:37 up 8 days, 21:24,  1 user,  load average: 2.45, 2.50, 2.25 e7d529bd7180df9791122d31d01b4c608cfa77ec

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -34,10 +34,11 @@ static struct rule {
         {"&&", logical_AND},                            //logical AND
         {"\\|\\|", logical_OR},                         //logical OR
         {"!", logical_NOT},                             //logical NOT
+        {"0[xX][A-Fa-f0-9]{1,8}",Hex},                  //hex
         {"\\$[a-dA-D][hlHL]|\\$[eE]?(ax|dx|cx|bx|bp|si|di|sp)", Register}, //register
         {"[a_zA_Z_][a-zA-Z0-9_]*", Variable},           //variable
         {"[0-9]{1,10}", Number},                        //nmumber
-        {"0[xX][A-Fa-f0-9]{1,8}", Hex},                 //hex
+        
 
 
 
